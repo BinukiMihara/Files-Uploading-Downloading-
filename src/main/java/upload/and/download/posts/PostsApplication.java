2f3744ef8e42179d973f01732619fb57c27fbef2
Binuki Mihara <133636115+BinukiMihara@users.noreply.github.com>
2025-03-27 16:40:43 +0530

    PostApplication

---
+++ src/main/java/upload/and/download/posts/PostsApplication.java
@@ -0,0 +1,13 @@
+package upload.and.download.posts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PostsApplication.class, args);
+	}
+
+}
