f5d7e59c7ca648a74dc3c727f3421debc18a073c
Binuki Mihara <133636115+BinukiMihara@users.noreply.github.com>
2025-03-27 16:40:14 +0530

    test

---
+++ src/test/java/upload/and/download/posts/PostsApplicationTests.java
@@ -0,0 +1,13 @@
+package upload.and.download.posts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
